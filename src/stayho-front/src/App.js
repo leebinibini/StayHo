9f860112fe11d5c0b0fe3d7d8989ddc57b9d96f9
gusdn0413 <gusdn0413@naver.com>
2024-08-09 14:43:11 +0900

    hotel/ShowOne.js reviewList 출력

--- src/stayho-front/src/App.js
+++ src/stayho-front/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ShowOne from './hotel/ShowOne';
+import {Routes, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path="/hotel/showOne/:hotelId" element={<ShowOne/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
