3c5377c30cc167b9c0303c9cbe98e56b3e122c3a
SongJIH <jh2song@naver.com>
2024-08-09 20:36:31 +0900

    재검색 가능

--- src/stayho-front/src/search/SearchResult.js
+++ src/stayho-front/src/search/SearchResult.js
@@ -2,13 +2,16 @@ import {Container} from "react-bootstrap";
 import SearchOption from "./SearchOption";
 import SearchList from "./SearchList";
 import SearchForm from "./SearchForm";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {useLocation} from "react-router-dom";
 
 let SearchResult=()=>{
     let location= useLocation()
     let hotels= location.state
     let [optionList, setOptionList]= useState(hotels)
+    useEffect(() => {
+        setOptionList(hotels)
+    }, [hotels]);
     return(
         <Container>
             <div className={'d-flex'}>
