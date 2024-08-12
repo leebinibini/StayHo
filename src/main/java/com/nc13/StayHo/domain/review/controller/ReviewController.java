683a5deb4fef58d21b013fd68c9b73786d3c4713
gusdn0413 <gusdn0413@naver.com>
2024-08-12 16:44:38 +0900

    평균, 검색

--- src/main/java/com/nc13/StayHo/domain/review/controller/ReviewController.java
+++ src/main/java/com/nc13/StayHo/domain/review/controller/ReviewController.java
@@ -35,20 +35,35 @@ public class ReviewController {
         return resultMap;
     }
 
+    @GetMapping("showAllByHotel/{hotelId}")
+    public HashMap<String, Object> showAllByHotel(@PathVariable int hotelId) {
+        HashMap<String, Object> resultMap = new HashMap<>();
+        List<Review> reviewList = reviewService.selectListByHotel(hotelId);
+        putResultMap(reviewList, resultMap);
+        return resultMap;
+    }
+
+    @GetMapping("/averageRating/{hotelId}")
+    public ResponseEntity<HashMap<String, Object>> getAverageRating(@PathVariable int hotelId) {
+        HashMap<String, Object> resultMap = new HashMap<>();
+        double averageRating = reviewService.averageRating(hotelId);
+        resultMap.put("averageRating", averageRating);
+        return ResponseEntity.ok(resultMap);
+    }
+
+    @GetMapping("/searchByComment")
+    public HashMap<String, Object> searchByComment(@RequestParam int hotelId, @RequestParam String keyword) {
+        HashMap<String, Object> resultMap = new HashMap<>();
+        List<Review> reviewList = reviewService.searchReviewsByComment(hotelId, keyword);
+        resultMap.put("reviewList", reviewList);
+        return resultMap;
+    }
+
     @GetMapping("showAllByRoom/{roomId}")
     public HashMap<String, Object> showAllByRoom(@PathVariable int roomId) {
         HashMap<String, Object> resultMap = new HashMap<>();
         List<Review> reviewList = reviewService.selectListByRoom(roomId);
-        List<ReviewSelectDTO> reviewDTOList = reviewList.stream()
-                .map(review -> new ReviewSelectDTO(
-                        review.getId(),
-                        review.getComment(),
-                        review.getRating(),
-                        review.getCreatedAt(),
-                        review.getUpdatedAt(),
-                        review.getMemberId()))
-                .collect(Collectors.toList());
-        resultMap.put("reviewList", reviewDTOList);
+        putResultMap(reviewList, resultMap);
         return resultMap;
     }
 
@@ -76,5 +91,19 @@ public class ReviewController {
     public ResponseEntity<Void> delete(@PathVariable int reviewId) {
         reviewService.delete(reviewId);
         return ResponseEntity.ok().build();
+    }
+
+    private static void putResultMap(List<Review> reviewList, HashMap<String, Object> resultMap) {
+        List<ReviewSelectDTO> reviewDTOList = reviewList.stream()
+                .map(review -> new ReviewSelectDTO(
+                        review.getId(),
+                        review.getComment(),
+                        review.getRating(),
+                        review.getCreatedAt(),
+                        review.getUpdatedAt(),
+                        review.getMemberId()))
+                .collect(Collectors.toList());
+
+        resultMap.put("reviewList", reviewDTOList);
     }
 }
