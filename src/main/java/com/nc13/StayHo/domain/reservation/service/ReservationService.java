a60f427b8295155200ec0539238b591a11ccb4ff
DESKTOP-0DUI2NJ\user <jungmin7315@kakao.com>
2024-08-12 11:01:19 +0900

    gitignore

---
+++ src/main/java/com/nc13/StayHo/domain/reservation/service/ReservationService.java
@@ -0,0 +1,14 @@
+package com.nc13.StayHo.domain.reservation.service;
+
+import com.nc13.StayHo.domain.reservation.domain.ReservationDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ReservationService {
+    List<ReservationDTO> selectAll(int member_id);
+    ReservationDTO selectOne(int id);
+    void update(ReservationDTO reservationDTO);
+    void delete(int id);
+}
---
+++ src/stayho-front/src/Reservation/user/ReservationInsert.js
@@ -0,0 +1,11 @@
+import {Container} from "react-bootstrap";
+
+let ReservationInsert = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default ReservationInsert
