a363efb8fa6658b4ef28e6933a1c85e48050d9d2
SongJIH <jh2song@naver.com>
2024-08-12 17:30:43 +0900

    hotel img 불러오기

---
+++ src/main/java/com/nc13/StayHo/domain/img/dto/HotelImgDTO.java
@@ -0,0 +1,18 @@
+package com.nc13.StayHo.domain.img.dto;
+
+import lombok.Data;
+
+@Data
+public class HotelImgDTO {
+    private int id;
+    private String filepath;
+    private String filename;
+    private int hotelId;
+
+    public HotelImgDTO(){}
+    public HotelImgDTO(String path, String name, int hotelId){
+        this.filepath= path;
+        this.filename= name;
+        this.hotelId= hotelId;
+    }
+}
---
+++ src/stayho-front/src/search/SearchResult.js
@@ -0,0 +1,24 @@
+import {Container} from "react-bootstrap";
+import SearchOption from "./SearchOption";
+import SearchList from "./SearchList";
+import {useEffect, useState} from "react";
+import {useLocation} from "react-router-dom";
+
+let SearchResult=()=>{
+    let location= useLocation()
+    let hotels= location.state.list
+    let images= location.state.images
+    let [optionList, setOptionList]= useState(hotels)
+    useEffect(() => {
+        setOptionList(hotels)
+    }, [hotels]);
+    return(
+        <Container>
+            <div className={'d-flex'}>
+            <SearchOption origin={hotels} hotels={optionList} setHotels={setOptionList}/>
+            <SearchList hotels={optionList} images={images}/>
+            </div>
+        </Container>
+    )
+}
+export default SearchResult
