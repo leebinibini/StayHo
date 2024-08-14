0e78a3a2432c73442ada71b57443cb7fa1aef7df
DESKTOP-0DUI2NJ\user <jungmin7315@kakao.com>
2024-08-14 13:15:39 +0900

    rename reservation

--- src/test/java/com/nc13/StayHo/StayHoApplicationTests.java
+++ src/test/java/com/nc13/StayHo/StayHoApplicationTests.java
@@ -2,6 +2,7 @@ package com.nc13.StayHo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class StayHoApplicationTests {
