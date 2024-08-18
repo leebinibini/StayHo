8d0c6a10daf1ca951a137ad30456cac63fd5d5de
gusdn0413 <gusdn0413@naver.com>
2024-08-18 17:21:50 +0900

    내 리뷰 관리

--- src/stayho-front/src/hotel/ShowList.js
+++ src/stayho-front/src/hotel/ShowList.js
@@ -48,6 +48,13 @@ let ShowList = () => {
         navigate('/registrant/reAuth');
     };
 
+    let onManageReviews = () => {
+        if (state !== null) {
+            let memberId = state.memberInfo.id;
+            navigate('/review/showAllByMember/' + memberId);
+        }
+    }
+
     useEffect(() => {
         let selectList = async () => {
             let resp = await axios
@@ -71,6 +78,8 @@ let ShowList = () => {
                     {state ? (
                         <>
                             <Button variant="outline-primary" className="mx-2" onClick={onMyPage}>마이페이지</Button>
+                            <Button variant="outline-secondary" className="mx-2" onClick={onManageReviews}>내 리뷰
+                                관리</Button>
                             <Button variant="outline-danger" className="mx-2" onClick={onLogOut}>로그아웃</Button>
                         </>
                     ) : (
